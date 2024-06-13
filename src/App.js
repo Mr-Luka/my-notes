8c855241657009d822f60f63333ad7638b146b22
Luka <luka.luka.luka93@gmail.com>
2024-06-13 13:09:38 -0700

    html is setup

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, {Component} from "react";
+
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>My Notes</h1>
+          <aside>
+            <button>+ New Note</button>
+            <input type="text" placeholder="Type here to search..."/>
+          </aside>
+        </header>
+        <ul>
+          <li>
+            <input type="text" placeholder="Title"/>
+            <textarea placeholder="Description..."/>
+            <span>X</span>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App;
